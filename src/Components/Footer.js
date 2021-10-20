d1d84ce8515a0a2298dd34ed19ecd7e0a3642f79
kadowning1 <kadowning1@gmail.com>
2021-10-20 19:42:50 -0400

    update about me

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+// import './App.css';
 
 export default function Footer() {
     return (
         <footer className="d-flex flex-wrap align-items-center py-3 my-4 border-top justify-content-center">
             <div className='text-center'>
+                <h6>Connect with Keith Downing</h6>
                 <ul className="nav col justify-content-center list-unstyled d-flex">
                     <div className="template-demo">
                         <a href='https://github.com/kadowning1' target='_blank' rel='noreferrer'>
@@ -19,7 +21,7 @@ export default function Footer() {
                         <a href='https://twitter.com/kadowning' target='_blank' rel='noreferrer'>
                             <button
                                 type="button" className="btn btn-social-icon btn-twitter btn-rounded">
-                                <i className="fa fa-twitter">
+                                <i className="fab fa-twitter">
                                 </i>
                             </button>
                         </a>
