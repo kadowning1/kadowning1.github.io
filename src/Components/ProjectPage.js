3cf53efee18c65ba88c0ffe3294cba6cc98d1172
kadowning1 <kadowning1@gmail.com>
2021-10-28 14:52:42 -0400

    update

--- src/Components/ProjectPage.js
+++ src/Components/ProjectPage.js
@@ -18,7 +18,7 @@ export default function AboutMe() {
             <br></br>
             <div className='row'>
                 <div className="col-md-6 col-sm-12 py-2 justify-content-center">
-                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white">
+                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white h-100">
                         <img className="card-img-top image-fluid mx-auto p-3" src='./img/Screen Shot 2021-10-01 at 4.06.36 PM.png' alt='food' />
                         <br></br>
                         <a href='https://github.com/kadowning1/weatherapp'><h5 class="card-title">Weather App</h5></a>
@@ -33,7 +33,7 @@ export default function AboutMe() {
                     </div>
                 </div>
                 <div className="col-md-6 col-sm-12 py-2 justify-content-center">
-                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white">
+                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white h-100">
                         <img className="card-img-top image-fluid mx-auto p-3" src='./img/Screen Shot 2021-09-23 at 11.17.24 AM.png' alt='food' />
                         <br></br>
                         <a href='https://github.com/kadowning1/bounce'><h5 class="card-title">Bouncing Balls</h5></a>
@@ -48,7 +48,7 @@ export default function AboutMe() {
                     </div>
                 </div>
                 <div className="col-md-6 col-sm-12 py-2 justify-content-center">
-                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white">
+                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white h-100">
                         <img className="card-img-top image-fluid mx-auto p-3" src='./img/Screen Shot 2021-09-22 at 11.09.29 AM.png' alt='food' />
                         <br></br>
                         <a href='https://github.com/kadowning1/alarmclock'><h5 class="card-title">Alarm Clock</h5></a>
@@ -63,7 +63,7 @@ export default function AboutMe() {
                     </div>
                 </div>
                 <div className="col-md-6 col-sm-12 py-2 justify-content-center">
-                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white">
+                    <div className="card card-body flex-column text-center p-3 mb-2 bg-secondary text-white h-100">
                         <img className="card-img-top image-fluid mx-auto p-3" src='./img/Screen Shot 2021-09-23 at 11.13.03 AM.png' alt='food' />
                         <br></br>
                         <a href='https://github.com/kadowning1/markdown-portfolio'><h5 class="card-title">MarkDown Project</h5></a>
