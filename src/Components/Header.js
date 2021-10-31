0df2df2aac73937c667df81b7ea8c3b2b91076db
kadowning1 <kadowning1@gmail.com>
2021-10-31 16:18:42 -0400

    update nav-bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import { Navbar, Nav, } from 'react-bootstrap'
 
 export default function Header() {
     return (
         <div>
-            <div className="container">
-                <header className="d-flex justify-content-center py-3 border-bottom">
-                    <ul className="nav nav-pills bg-secondary">
-                        <li className="nav-item"><a href="https://github.com/kadowning1" target='_blank' rel='noreferrer' className="nav-link text-white">Home</a></li>
-                        <li className="nav-item"><a href="https://github.com/kadowning1" target='_blank' rel='noreferrer' className="nav-link text-white" aria-current="page">About</a></li>
-                        <li className="nav-item"><a href="https://github.com/kadowning1" target='_blank' rel='noreferrer' className="nav-link text-white">Blog</a></li>
-                        <li className="nav-item"><a href="https://github.com/kadowning1" target='_blank' rel='noreferrer' className="nav-link text-white">Portfolio</a></li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                        <button className="btn btn-outline-secondary" type="submit">Search</button>
-
-                    </form>
-                </header>
+            <div className="row justify-content-center text-center">
+                <div className="col-md-12 justify-content-center text-center">
+                    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className='justify-content-center'>
+                        <Navbar.Brand href="#home">Keith Downing - Web Developer</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="/about-us">About Me</Nav.Link>
+                                <Nav.Link href="/about-us">Projects</Nav.Link>
+                                <Nav.Link href="/contact-us">Blog</Nav.Link>
+                                <Nav.Link href="/contact-us">Contact Me</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
             </div>
         </div>
     )
+
 }
 
