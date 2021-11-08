33e700d01c5e0f9a92722e84da495437c3a62e7b
kadowning1 <kadowning1@gmail.com>
2021-11-08 15:12:16 -0500

    update blog

--- src/Components/BlogPosts/Week11.js
+++ src/Components/BlogPosts/Week11.js
@@ -10,20 +10,20 @@ export default function Week11() {
                             class="rounded mx-auto d-block img-fluid img-thumbnail w-50 p-2" alt="..." />
                         <div class="card-body text-white">
                             <h5 class="card-title text-white">Week 1 Back-End</h5>
-                            <p class="card-text">Week 6 of Front-End of the Bootcamp is complete.  I feel like this was a big week for me.
-                            Oftentimes throughout the Bootcamp,
-                            I felt like I have struggled to learn concepts and implement them.
-                            With this struggle, my confidence has been up and down each week.</p>
+                            <p class="card-text">Week 11 of the Bootcamp presented the same challenges as the previous weeks.  This week, we learned about PHP and SQL for the first time, 
+                            and with that came a few more highs and lows.  PHP is a completely new concept so it's amazing to see the similarities between it and Javascript.  The logic is the same, 
+                            but the syntax is an adjustment.  
+                            I caught on relatively quickly and I have begun to look forward to other languages and frameworks to learn to continue to grow.</p>
 
-                            <p>This week our instructors introduced React Hooks and the beginnings of the back-end process of web development.
-                            My project was to complete a user authentication app to grab data from an API and log in to the user successfully.
-                            I feel like I gravitated toward this project because it was very hands-on with data and adding it to my app.
-                                With the back-end learning starting in full force next week, I can't wait to use, store, and access data that the user is constantly providing.</p>
+                            <p>Our project this week consisted of using PHP to alter data in a SQL table.  
+                                It was very insightful to see how they are connected and how to run specific queries to get the information that is needed.  
+                                Once I was able to get one query fully functional, the project came together quickly.  I'm excited to put all of our "pieces" together to create our final 
+                                project at the end of Bootcamp.  
+                                The camp is flying by while I'm also learning so much! </p>
 
-                            <p>At the end of this week, we have learned different aspects of React for the past three weeks.  From creating classes to functions to hooks to routing,
-                            I have learned so much about creating apps with React in such a short amount of time.
-                            With only six weeks left, the camp is flying by and our attention will now turn to
-                                managing the new concepts with trying to look forward to our job search at the end of camp.</p>
+                            <p>A challenge for me for the last few weeks of camp will be a mental hurdle.  I'm going to need to 
+                                balance learning new concepts, with delivering the projects, while also thinking about obtaining a job once the Bootcamp is complete.  
+                                I don't want to get ahead of myself, but also don't want to fall behind in anything.  It will take some focus and discipline, but I'm up for the challenge.</p>
                         </div>
                         <div class="card-footer">
                             <small class="text-center">Week of November 1 to November 7, 2021</small>
