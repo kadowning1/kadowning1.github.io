469b50af42188edce3be1890807f4ba7d9cb7188
kadowning1 <kadowning1@gmail.com>
2022-08-04 11:51:19 -0400

    GA work with Debug

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -13,16 +13,6 @@ export const Blog = () => {
                     <h1 className='text-center'>Blog Posts</h1>
                     <BlogPost />
                     <Week1 />
-                    {/* <Week2 />
-                    <Week3 />
-                    <Week4 />
-                    <Week5 />
-                    <Week6 />
-                    <Week7 />
-                    <Week8 />
-                    <Week9 />
-                    <Week10 />
-                    <Week11 /> */}
                 </div>
             </div>
         </>
