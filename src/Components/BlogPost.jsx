2d0a31fb7b46c75becdf424e3501252e3bc97460
kadowning1 <kadowning1@gmail.com>
2022-08-04 19:30:42 -0400

    refactor work

--- src/Components/BlogPost.jsx
+++ src/Components/BlogPost.jsx
@@ -1,78 +1,50 @@
-import React from 'react';
-import person from '../blogpost.json';
+import React, { useState } from 'react';
+import blog from '../blogpost.json';
+import { Button, Collapse, Col, Row, Container } from 'react-bootstrap';
 
 export default function BlogPost() {
-  console.log(person);
-  console.log(person[0].headline);
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className='container'>
-      <div className='row p-3'>
-        <div className='clearfix'>
-          {person.map((person, index) => (
-            <div className='col-md-4 my-3' key={index}>
-              <div className='card position-relative top-50 start-50 translate-middle bg-secondary p-3'>
-                <div className='card-body text-white'>
-                  <img
-                    src='/img/Markdown.png'
-                    className='rounded mx-auto d-block img-fluid img-thumbnail p-2'
-                    alt='...'
-                  />
-                  <h5 className='card-title text-white pt-2'>{person.headline}</h5>
-                  <p className='card-text'>{person.p2}</p>
-                  <p className='card-text'>{person.p3}</p>
-                  <a href={person.link} className='btn btn-primary'>
-                    Read More
-                  </a>
-                </div>
-                <div className='card-footer'>
-                  <small className='text-center'>
-                    Week of August 23 to August 29, 2021
-                  </small>
-                </div>
+    <Container className=''>
+      <Row className='p-3'>
+        {blog.map((blog, index) => (
+          <Col className='my-3 d-flex' key={index} sm={12}>
+            <div className='card position-relative top-50 start-50 translate-middle bg-secondary p-3'>
+              <div className='card-body text-white'>
+                <img
+                  src={blog.image}
+                  className='rounded mx-auto d-block img-fluid img-thumbnail p-2'
+                  alt={blog.title}
+                />
+                <h5 className='card-title text-white pt-2'>{blog.headline}</h5>
+                <p className='card-text'>{blog.p1}</p>
+                {!open && (
+                  <Col small={12} className='text-center'>
+                    <Button
+                      onClick={() => setOpen(!open)}
+                      aria-controls='example-collapse-text'
+                      aria-expanded={open}
+                      variant='outline-light'
+                    >
+                      More
+                    </Button>
+                  </Col>
+                )}
+                <Collapse in={open}>
+                  <div id='example-collapse-text' className='card-text'>
+                    {blog.p2}
+                    {blog.p3}
+                  </div>
+                </Collapse>
+              </div>
+              <div className='card-footer'>
+                <small className='text-center'>{blog.date}</small>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
-//           <div className='card position-relative top-50 start-50 translate-middle bg-secondary p-3'>
-//             <div className='card-body text-white'>
-//               <h5 className='card-title text-white'>Week 1 Onboarding</h5>
-//               <p className='card-text'>
-//                 Creating my first blog post during the onboarding stages of the
-//                 Web Dev bootcamp. I have been learning up to this point to
-//                 seamlessly enter the bootcamp as prepared as possible while also
-//                 leaving myself opportunities to see how others can teach me
-//                 certain aspects. I'm trying to combine many avenues (SoloLearn,
-//                 FreeCodeCamp, Eloquent <i>Javascript</i> and Prework) to be in a
-//                 position to approach the camp full steam ahead.
-//               </p>
-
-//               <p>
-//                 The first week has given me all the emotions that I've expected.
-//                 Excitement, nervousness, anxiousness, and a sense of purpose
-//                 knowing I'm on the right track. I've enjoyed all of the
-//                 headaches and processes of the first week especially with{' '}
-//                 <i>Git</i> and <i>GitHub</i>. I know it is not going to be easy,
-//                 but I can't wait to build on skills each day. I'm looking
-//                 forward to grasping <i>JavaScript</i> as well as continuing
-//                 building on my introductory <i>Git</i> skills. Additionally in
-//                 talking to mentors in the field, I'm eager to learn more on the{' '}
-//                 <i>SQL</i> and Backend side of the entire stack. Let the fun
-//                 begin!
-//               </p>
-
-//               <p>
-//                 {' '}
-//                 I will continue to add to this page frequently so be on the
-//                 lookout for more content ahead!
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
